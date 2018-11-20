5320be327bd12111a73ffaaac6a8175c0321a757
Pavel Balusek <mr.p.balusek@gmail.com>
2018-11-20 22:23:17 +0100

    Project start

---
+++ src/control/Controller.java
@@ -0,0 +1,12 @@
+package control;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label title;
+    public Button addAngle;
+
+    public Controller(){
+    }
+}
